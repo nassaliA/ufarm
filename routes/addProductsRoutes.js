a8bce36e9e8914cbeee63b196bae2455e131af4c
nassaliA <nassali.aminah7@gmail.com>
2023-04-25 20:51:36 +0300

    first commit

--- routes/addProductsRoutes.js
+++ routes/addProductsRoutes.js
@@ -1,44 +1,44 @@
-// const express = require("express");
-// const router = express.Router();
-// const multer = require("multer");
-// const path = require("path");
-// //const Product = require("../models/urbanFarmer/addProductModel");
-// //const requireAuth = require("../middleware/auth");
-// router.use(express.static(path.join(__dirname, "public")));
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const path = require("path");
+//const Product = require("../models/urbanFarmer/addProductModel");
+//const requireAuth = require("../middleware/auth");
+router.use(express.static(path.join(__dirname, "public")));
 
-// // Defining Multer storage configuration
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, path.join(__dirname, "../public/assets/uploads"));
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, file.originalname);
-//   },
-// });
+// Defining Multer storage configuration
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../public/assets/uploads"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 
-// // Create Multer upload instance
-// const upload = multer({ storage: storage });
+// Create Multer upload instance
+const upload = multer({ storage: storage });
 
-// //router.get("/", requireAuth, (req, res) => {
-//   res.render("addProduct.pug");
-// //});
+//router.get("/", requireAuth, (req, res) => {
+  res.render("addProducts");
+//});
 
-// router.post("/", requireAuth, upload.single("image"), async (req, res) => {
-//   const filename = req.file.filename;
-//   const product_fields = req.body;
-//   product_fields.image = filename;
-//   product_fields.is_approved = false;
-//   product_fields.farmer_details = req.session.user;
-//   try {
-//     const product = new Product(product_fields);
-//     product.save();
-//     res.render("success.pug", {
-//       message: "Product successfully added",
-//       page: "add-product",
-//     });
-//   } catch (error) {
-//     res.send({ error });
-//   }
-// });
+router.post("/", requireAuth, upload.single("image"), async (req, res) => {
+  const filename = req.file.filename;
+  const product_fields = req.body;
+  product_fields.image = filename;
+  product_fields.is_approved = false;
+  product_fields.farmer_details = req.session.user;
+  try {
+    const product = new Product(product_fields);
+    product.save();
+    res.render("success.pug", {
+      message: "Product successfully added",
+      page: "add-product",
+    });
+  } catch (error) {
+    res.send({ error });
+  }
+});
 
-// module.exports = router;
+module.exports = router;
